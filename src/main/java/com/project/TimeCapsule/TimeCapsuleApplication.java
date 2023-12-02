592430f67afa202416f98bc0900438800d4dcae4
Yulia <ybe2004ybe@gmail.com>
2023-12-02 09:17:49 +0200

    Edits for deployment

--- src/main/java/com/project/TimeCapsule/TimeCapsuleApplication.java
+++ src/main/java/com/project/TimeCapsule/TimeCapsuleApplication.java
@@ -2,14 +2,20 @@ package com.project.TimeCapsule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.project.TimeCapsule", "com.project.TimeCapsule.security"})
-public class TimeCapsuleApplication {
+public class TimeCapsuleApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TimeCapsuleApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TimeCapsuleApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(TimeCapsuleApplication.class, args);
+    }
 }
